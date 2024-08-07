c7fc72963562d8c914d9ca335d50c82fc1a721b4
Ayesha <ayeshasolanki151@gmail.com>
2024-08-08 00:38:17 +0530

    cors enabled

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,13 @@ import { Hono } from "hono";
 
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
 // this generic is used in hono because env type is not defined
+
+app.use("/*", cors());
 
 app.route("/api/v1/user", userRouter);
 
